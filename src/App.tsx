534ba952d042812c8720a3e8cb50e5dedf9168b7
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-07-01 15:44:41 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import MovieList from "./components/MovieList";
+import MovieEditor from "./components/MovieEditor";
+import reducers from "./reducers";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <div className="container">
+        <h1>Popular movies</h1>
+
+        <BrowserRouter>
+          <Switch>
+            <Route path="/movies">
+              <MovieList />
+            </Route>
+            <Route path="/movie/:id">
+              <MovieEditor />
+            </Route>
+            <Route>
+              <Redirect to="/movies" />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
